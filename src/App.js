965915bff7e8eeb15834c5056e848df1cc6c4671
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-10-25 16:51:39 +0200

    es3

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import CustomNavbar from './Components/CustomNavbar'
 import MyFooter from './Components/MyFooter'
 import AdditionalContentExample from './Components/AdditionalContentExample'
-import BasicCard from './Components/BasicCard'
+
+// import SingleBook from './Components/SingleBook'
+import BookList from './Components/BookList'
+import fantasy from './data/fantasy.json'
+import h from './data/history.json'
 
 function App() {
   return (
     <div>
       <CustomNavbar addText="Mondadori" />
-
       <AdditionalContentExample />
-      <BasicCard />
+      {/* <SingleBook
+        img="https://images-na.ssl-images-amazon.com/images/I/51eHtkVLL5L.jpg"
+        title="The Last Wish: Introducing the Witcher"
+      /> */}
+      <BookList books={h} />
       <MyFooter />
     </div>
   )
